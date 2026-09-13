d4a58efdf02c8ef7df9592b77efb1b91f988d0cb
rufaida akhter akhi <rufaida.formal@gmail.com>
2026-09-13 21:49:01 +0600

    ok

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Footer from "./components/footer"
+import Nav from "./components/nav"
+
+
+function App() {
+  
+
+  return (
+    <>  
+    <Nav />
+    <Footer />
+    </>
+  )
+}
+
+export default App
